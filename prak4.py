0f9b951c8a794d60394c5ab5cf7eb014ce21e7f4
Machichka <macickamihajlo@gmail.com>
2024-11-27 23:01:48 +0200

    Update prak4.py

--- prak4.py
+++ prak4.py
@@ -8,7 +8,7 @@ def sort_list(unique_list):
     return numbers + strings
 
 
-input_list = [3, 1, 2, 3, 4, 5, 6, 3, 4, 5, 7, 6, 5, 4, 3, 4, 5, 4, 3, 'Привіт', 'анаконда']
+input_list = [3, 1, 2, 3, 4, 5, 6, 3, 4, 5, 7, 6, 5, 4, 3, 4, 5, 4, 3, 'Привіт', 'Анаконда']
 unique_list = remove_duplicates(input_list)
 print("Список без повторень:", unique_list)
 
