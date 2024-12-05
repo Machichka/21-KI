7ca46c78d32de4734f9ec286195fb840396fd9d7
Machichka <macickamihajlo@gmail.com>
2024-12-05 14:04:44 +0200

    Update prak5.py

--- prak5.py
+++ prak5.py
@@ -2,7 +2,7 @@ import telebot
 bot = telebot.TeleBot("7860026666:AAHNNIkLieuOnbhf5pkfIxmtgb10XNlje2M")
 
 
-@bot.message_handler(func=lambda message: True)
+bot.message_handler(content_types=['text'])
 def echo_all(message):
     bot.reply_to(message, message.text)
 
